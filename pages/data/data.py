d624b293bcd15196196697b42ff717365de7b2f6
A_sitoula <arp2ctaula@gmail.com>
2025-02-17 23:28:10 +0100

    path fixed

--- pages/data/data.py
+++ pages/data/data.py
@@ -1,16 +1,17 @@
-# load_data.py
 import pandas as pd
+import os
 
 def load_data():
-    carbon_emissions = pd.read_csv(r"C:\Users\arpan\Climate-Change-Tracker-Dashboard\Datasets\annual-co2-emissions-per-country\annual-co2-emissions-per-country.csv")
-    annual_temp = pd.read_csv(r"C:\Users\arpan\Climate-Change-Tracker-Dashboard\Datasets\annual-temperature-anomalies\annual-temperature-anomalies.csv")
-    ghg_emissions = pd.read_csv(r"C:\Users\arpan\Climate-Change-Tracker-Dashboard\Datasets\ghg-emissions-by-gas\ghg-emissions-by-gas.csv")
-    data_iso = pd.read_csv(r"C:\Users\arpan\Climate-Change-Tracker-Dashboard\Datasets\Global Primary Forest loss\iso_metadata.csv")
-    energy_subs = pd.read_csv(r"C:\Users\arpan\Climate-Change-Tracker-Dashboard\Datasets\global-energy-substitution\global-energy-substitution.csv")
-    sea_level = pd.read_csv(r"C:\Users\arpan\Climate-Change-Tracker-Dashboard\Datasets\sea-level\sea-level.csv")
-    disaster = pd.read_csv(r"C:\Users\arpan\Climate-Change-Tracker-Dashboard\Datasets\disaster.csv")
+    # Go up one level and resolve to the absolute path of 'Datasets'
+    base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..', 'Datasets'))
 
-
-    
+    # Now, use this base path to reference the datasets
+    carbon_emissions = pd.read_csv(os.path.join(base_path, "annual-co2-emissions-per-country/annual-co2-emissions-per-country.csv"))
+    annual_temp = pd.read_csv(os.path.join(base_path, "annual-temperature-anomalies/annual-temperature-anomalies.csv"))
+    ghg_emissions = pd.read_csv(os.path.join(base_path, "ghg-emissions-by-gas/ghg-emissions-by-gas.csv"))
+    data_iso = pd.read_csv(os.path.join(base_path, "Global Primary Forest loss/iso_metadata.csv"))
+    energy_subs = pd.read_csv(os.path.join(base_path, "global-energy-substitution/global-energy-substitution.csv"))
+    sea_level = pd.read_csv(os.path.join(base_path, "sea-level/sea-level.csv"))
+    disaster = pd.read_csv(os.path.join(base_path, "disaster.csv"))
 
     return carbon_emissions, annual_temp, ghg_emissions, data_iso, energy_subs, sea_level, disaster
